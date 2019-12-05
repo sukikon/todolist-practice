10ff1e1dce215f30c251f3c96f157adfbd66424a
sukikon <31767696+sukikon@users.noreply.github.com>
2019-12-05 15:51:04 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,137 +1,80 @@
-function addTodolist(e) {
-    var obj_list = {
-        todo: "",   //data
-        done: false     //state
-    };
-    document.getElementById("add_list").value = document.getElementById("add_list").value.trim();
+function get_todo() {
+    var todo = new Array;
+    var todoString = localStorage.getItem('todo');
+    if (todoString !== null) {
+        todo = JSON.parse(todoString);  //change to JsonString
+    }
+    return todo;
+}
+function get_fin(){
+    var fin = new Array;
+    var finString = localStorage.getItem('fin');
+    if(finString !== null){
+        fin = JSON.parse(finString);
+    }
+    return fin;
+}
+ 
+function add() {
+    var text = document.getElementById('text').value;
+    var todo = get_todo();  //Json
+    if(text.length == 0){
+        alert("enter something please")
+        return
+    }
+    todo.push(text);
+    localStorage.setItem('todo', JSON.stringify(todo));
+ 
+    show();
+ 
+    return false;
+}
+
+ 
+function done() {
+    var id = this.getAttribute('id');
+    var todo = get_todo();
+    var fin = get_fin();
+
+    fin.push(todo[id]);
+    todo.splice(id, 1);
+
+    localStorage.setItem('todo', JSON.stringify(todo));
+    localStorage.setItem('fin', JSON.stringify(fin));
     
-    if (document.getElementById("add_list").value.length === 0){
-        alert("please enter something");
-        return;
-    }
  
-    obj_list.todo = document.getElementById("add_list").value;
-    todolist.push(obj_list);
+    show();
  
-    saveData(todolist);
- 
-    document.getElementById("add_list").value = "";     //初始化输入框
-    load();     //将用户输入的数据添加至dom节点
-    document.getElementById("add_list").focus();
+    return false;
 }
-function load(){
-    var todo = document.getElementById("todolist"),
-        done = document.getElementById("donelist"),
-        todocount = document.getElementById("todocount"),
-        donecount = document.getElementById("donecount"),
-        todoString = "",
-        doneString = "",
-        todoCount = 0,
-        doneCount = 0;
-    document.getElementById("add_list").focus();
  
-    todolist = loadData();
+function show() {
+    var todo = get_todo();
+    var fin = get_fin();
  
-    //todolist数组对象里若包含用户输入数据，则将其添加至dom节点；若为空对象，则初始化页面。
-    if (todolist != null){
-        for (var i=0; i<todolist.length; i ++){
-            if(!todolist[i].done){
-                todoString += "<li>"
-//通过onchange事件，复选框值有改变则调用update函数，并改变输入数据“done”属性的布尔值，这样
-//下次load()后，这段数据会进入不同的分组，未完成的事项分入已完成事项组，已完成事项分入未完成事项组
-//点击事项调用edit函数
-//点击“-”，调用remove函数
-                    + "<input type='checkbox' οnchange='update("+i+", \"done\", true)'>"
-                    + "<p id='p-"+i+"' οnclick='edit("+i+")'>" + todolist[i].todo + "</p>" +
-                    "<a οnclick='remove("+i+")'>-</a>" +
-                    "</li>";    //将每次用户输入的数据，通过节点<p>利用id标记，以便后续编辑功能定位
-                todoCount ++;
-            }
-            else{
-                doneString += "<li>"
-                    + "<input type='checkbox' "
-                    + "οnchange='update("+i+", \"done\", false)' checked>"
-                    + "<p id='p-"+i+"' οnclick='edit("+i+")'>" + todolist[i].todo + "</p>"
-                    + "<a οnclick='remove("+i+")'>-</a>"
-                    + "</li>";
-                doneCount ++;
-            }
-        }
+    var html = '<ul>';
+    for(var i=0; i<todo.length; i++) {
+        html += '<li><input type="checkbox" name="done" id="' + i  + '">' + todo[i] + '</li>';
+    };
+    html += '</ul>';
+
+    var htmlf = '<ul>';
+    for(var i=0; i<fin.length; i++) {
+        htmlf += '<li><input type="checkbox" name="done" checked="true" id="' + i  + '">' + fin[i] + '</li>'
+    };
+    htmlf += '</ul>';
+
+    //<input type="checkbox" name="" id="">
  
-        todo.innerHTML = todoString;
-        done.innerHTML = doneString;
-        todocount.innerHTML = todoCount;
-        donecount.innerHTML = doneCount;
-    }
-    else {
-        todo.innerHTML = "";
-        done.innerHTML = "";
-        todocount.innerHTML = 0;
-        donecount.innerHTML = 0;
-    }
+    document.getElementById('todo').innerHTML = html;
+    document.getElementById('fin').innerHTML = htmlf;
+ 
+    var buttons = document.getElementsByName('done');
+    
+    for (var i=0; i < buttons.length; i++) {
+        buttons[i].addEventListener('change', done)
+    };
 }
-function edit(i) {
-    var p = document.getElementById('p-' + i),
-        pContent = p.innerHTML,
-        inputId;
- 
-//通过upadate函数对todolist数组相应项进行更新，将用户输入的内容写入到todolist数组相应项的todo属性中
-    function confirm() {
-        if (inputId.value.length === 0) {
-            p.innerHTML = pContent;
-            alert("内容不能为空");
-        }
-        else {
-            update(i, "todo", inputId.value);   //修改事项内容后，更新数组里对应项"todo"属性的值，以便更新dom节点
-        }
-    }
- 
-//结合keypress事件，按下enter键，调用confirm函数
-    function enter(e) {
-        if (e.keyCode == 13){
-            confirm();
-        }
-    }
- 
-    p.innerHTML = "<input type='text' id='input-"+i+"' value='"+pContent+"'>";
-    inputId = document.getElementById('input-'+i);
-    inputId.focus();
-    inputId.setSelectionRange(0, inputId.value.length);
-    inputId.onblur = confirm;   //表单控件失去焦点，调用confirm函数，即对页面内容进行更新
-    inputId.onkeypress = enter;     //对按键事件进行监控
-} 
-function update(i, field, value) {
-    todolist[i][field] = value;
-    saveData(todolist);
-    load();
-}
-function remove(i) {
-    todolist.splice(i, 1);
- 
-    saveData(todolist); //相同名称的缓存会覆盖，更新缓存
- 
-    load();
-}
-function saveData(data) {
-    localStorage.setItem("mytodolist", JSON.stringify(data));   //JS对象转换成JSON对象存进本地缓存
-}
-function loadData() {
-    var hisTory = localStorage.getItem("mytodolist");
-    if(hisTory !=null){
-        return JSON.parse(hisTory);     //JSON对象转换为JS对象
-    }
-    else {
-        return []; 
-    }
-}
-function clear() {
-    localStorage.clear();
-    load();
-}
-window.addEventListener("load", load);  //页面加载完毕调用load函数
-document.getElementById("clearbutton").onclick = clear;
-document.getElementById("add_list").onkeypress = function (event) {
-    if(event.keyCode === 13){
-        addTodolist();
-    }
-};
+
+document.getElementById('add').addEventListener('click', add);
+show();
